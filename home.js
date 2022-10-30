c2b6199e8bd9a53b244d70f1b48b0a6405bd565e
Astor Almeida <astormg@gmail.com>
2022-10-30 17:46:37 -0300

    Page Login

---
+++ home.js
@@ -0,0 +1,27 @@
+        var accessData = [];
+
+        function users() {  accessData = JSON.parse(this.responseText); }
+        
+        function error(err) { console.log('Erro:', err); }
+        
+        var xhr = new XMLHttpRequest();
+        xhr.onload = users;
+        xhr.onerror = error;
+        xhr.open('GET', "access.json", false);
+        xhr.send();
+        
+        function validate() {
+                let door = "locked";
+                let nameUser = document.getElementById("login").value;
+                let passwordUser = document.getElementById("password").value;
+
+                accessData.map((i) => {
+                        if (nameUser == i.login) {
+                                if (passwordUser == i.password) {
+                                        door = "open"
+                                } else { door = "close"}
+                        }
+                }
+        )}
+
+        
